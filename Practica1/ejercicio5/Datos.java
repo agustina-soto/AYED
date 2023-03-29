75b4ec402681612bb7122b6852eb1d7fc6598b26
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-03-29 18:16:58 -0300

    Create Datos.java

---
+++ Practica1/ejercicio5/Datos.java
@@ -0,0 +1,34 @@
+package ejercicio5;
+
+public class Datos {
+
+	private int min;
+	private int max;
+	private float promedio;
+
+
+	public int getMin() {
+		return min;
+	}
+
+	public void setMin(int min) {
+		this.min = min;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public void setMax(int max) {
+		this.max = max;
+	}
+
+	public float getPromedio() {
+		return promedio;
+	}
+
+	public void setPromedio(float promedio) {
+		this.promedio = promedio;
+	}
+
+}
