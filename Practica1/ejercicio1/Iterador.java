058e2da7ec93d7dc0cc682d567fcb0b69a6ca1d9
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-03-29 11:11:02 -0300

    Create Iterador.java

---
+++ Practica1/ejercicio1/Iterador.java
@@ -0,0 +1,24 @@
+public class Iterador {
+
+	public static void iterarFor(int a, int b) {
+		for (int i = a; i <= b; i++) {
+			System.out.println(i);
+		}
+	}
+	
+	public static void iterarWhile(int a, int b) {
+		int i = a;
+		while (i <= b) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+	public static void iterarRecursivo(int a, int b) {
+		System.out.println(a);
+		if (b > a) {
+			iterarRecursivo(a+1, b);
+		}
+	}
+
+}
