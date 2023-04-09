3b1bff45d6a126c24e632f367a3746ba787d7a33
Agustina Soto <89858875+agustina-soto@users.noreply.github.com>
2023-04-09 01:41:19 -0300

    Update Ejercicio1_6.java

--- Practica 2/Ejercicio1/Ejercicio1_6.java
+++ Practica 2/Ejercicio1/Ejercicio1_6.java
@@ -1,16 +1,13 @@
 package ejercicio1;
 
 import tp02.ejercicio1.*;
-import java.util.Scanner;
 
 public class Ejercicio1_6 {
 
 	public static void main(String[] args) {
-		System.out.println("Ingrese un numero");
-		Scanner s = new Scanner(System.in);
 		Sucesion sucesion = new Sucesion();
 
-		ListaDeEnterosEnlazada resultado = sucesion.calcularSucesion(s.nextInt());
+		ListaDeEnterosEnlazada resultado = sucesion.calcularSucesion(5);
 		
 		resultado.comenzar();
 		ImpresionInversaRecursiva.imprimirInversaRecursiva(resultado, resultado.tamanio());
